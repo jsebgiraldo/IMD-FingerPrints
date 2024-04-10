4e8e71298fec8f0ac6d1d20656e8be5a1de6cfbe
jsebgiraldo <jsgiraldod@hotmail.com>
2024-04-10 13:54:20 +0800

    feat: Inital layout was created         Migration of the ui was carried out

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.resize(570,600);
     w.show();
     return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,33 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->statusbar->addWidget(new QLabel("Status: "));
+    ui->statusbar->addWidget(new QLabel("Disconnected"));
+
+    QTabWidget *tabw = ui->tabWidget;
+    tabw->setTabText(0, "");
+    tabw->setTabText(1, "");
+    tabw->setTabText(2, "");
+    tabw->setTabText(3, "");
+    QTabBar *tabbar = tabw->tabBar();
+
+    QLabel *lbl1 = new QLabel();
+    lbl1->setText("Capture");
+    tabbar->setTabButton(0, QTabBar::LeftSide, lbl1);
+
+    QLabel *lbl2 = new QLabel();
+    lbl2->setText("Enroll");
+    tabbar->setTabButton(1, QTabBar::LeftSide, lbl2);
+
+    QLabel *lbl3 = new QLabel();
+    lbl3->setText("Authentication");
+    tabbar->setTabButton(2, QTabBar::LeftSide, lbl3);
+
+    QLabel *lbl4 = new QLabel();
+    lbl4->setText("Vertification");
+    tabbar->setTabButton(3, QTabBar::LeftSide, lbl4);
+
 }
 
 MainWindow::~MainWindow()
